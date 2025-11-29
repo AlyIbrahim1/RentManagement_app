2503adae80a23d4b678942923b76caed53c39ca7
Aly Lotfy <95508576+AlyIbrahim1@users.noreply.github.com>
2025-11-29 22:20:03 +0200

    Feat: Add basic framework for customtkinter GUI

---
+++ src/app.py
@@ -0,0 +1,15 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("1000x650")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=20, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Rent Managemennt System", font=("Roboto", 24))
+label.pack(pady=20, padx=10)
+
+root.mainloop()
