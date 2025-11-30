1fb9b020c72bb71826b3afde090cc496cbb7e6d1
Aly Lotfy <95508576+AlyIbrahim1@users.noreply.github.com>
2025-12-01 00:07:45 +0200

    Feat: Setup and add basic functions to database file
    Connect database.py file to database and setup basic functions to create table, add records, delete records, and clear table

--- src/database.py
+++ src/database.py
@@ -1,1 +1,43 @@
 import sqlite3
+
+conn = sqlite3.connect("data/rent_management.db") # connects to database file
+cursor = conn.cursor() # creates a cursor
+
+# creates the database table
+cursor.execute("""
+    --sql
+    CREATE TABLE IF NOT EXISTS renters (
+        appartmentNumber INTEGER PRIMARY KEY,
+        name TEXT,
+        rentAmount INTEGER,
+        lastMonthPayed TEXT,
+        unpaidMonths INTEGER,
+        rentDue INTEGER;
+    ) 
+""")
+conn.commit()
+
+def addRecord(appartmentNumber, name, rentAmount, lastMonthPayed, unpaidMonths, rentDue):
+    cursor.execute("""
+        --sql 
+        INSERT INTO renters VALUES (?,?,?,?,?)
+        ;"""
+        , 
+        (appartmentNumber, name, rentAmount, lastMonthPayed, unpaidMonths, rentDue)
+    )
+    conn.comit()
+
+def deleteRecord(appartmentNumber):
+    cursor.execute(--"""
+    --sql
+    DELETE FROM renters WHERE appartmentNumber = (?)
+    ;
+    """
+    , appartmentNumber
+    )
+
+def clearTable():
+    cursor.execute("DROP TABLE renters")
+    conn.commit()
+
+conn.close()
