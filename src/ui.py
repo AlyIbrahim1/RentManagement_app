3da5718a7bf739db2d33a2e57708ba09c63f9d9b
Aly Lotfy <95508576+AlyIbrahim1@users.noreply.github.com>
2025-12-03 17:45:59 +0200

    Refactor: changed root to window for a more context friendly naming convention

--- src/ui.py
+++ src/ui.py
@@ -4,13 +4,13 @@ from . import database
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("dark-blue")
 
-root = customtkinter.CTk()
-root.geometry("1000x650")
+window = customtkinter.CTk()
+window.geometry("1000x650")
 
-frame = customtkinter.CTkFrame(master=root)
+frame = customtkinter.CTkFrame(master=window)
 frame.pack(pady=20, padx=20, fill="both", expand=True)
 
 label = customtkinter.CTkLabel(master=frame, text="Rent Management System", font=("Roboto", 24))
 label.pack(pady=20, padx=10)
 
-root.mainloop()
+window.mainloop()
