2e009d7aeff189da8c487cfea41b710c8ce6772f
Aly Lotfy <95508576+AlyIbrahim1@users.noreply.github.com>
2025-12-03 15:11:47 +0200

    refactor: Clean SQL formatting

--- src/database.py
+++ src/database.py
@@ -4,40 +4,30 @@ conn = sqlite3.connect("data/rent_management.db") # connects to database file
 cursor = conn.cursor() # creates a cursor
 
 # creates the database table
-cursor.execute("""
-    --sql
-    CREATE TABLE IF NOT EXISTS renters (
+cursor.execute("""CREATE TABLE IF NOT EXISTS renters (
         appartmentNumber INTEGER PRIMARY KEY,
         name TEXT,
         rentAmount INTEGER,
         lastMonthPayed TEXT,
         unpaidMonths INTEGER,
-        rentDue INTEGER;
-    ) 
+        rentDue INTEGER
+    ); 
 """)
 conn.commit()
 
 def addRecord(appartmentNumber, name, rentAmount, lastMonthPayed, unpaidMonths, rentDue):
-    cursor.execute("""
-        --sql 
-        INSERT INTO renters VALUES (?,?,?,?,?)
-        ;"""
-        , 
-        (appartmentNumber, name, rentAmount, lastMonthPayed, unpaidMonths, rentDue)
+    cursor.execute("""INSERT INTO renters VALUES (?,?,?,?,?);"""
+        , (appartmentNumber, name, rentAmount, lastMonthPayed, unpaidMonths, rentDue)
     )
     conn.comit()
 
 def deleteRecord(appartmentNumber):
-    cursor.execute(--"""
-    --sql
-    DELETE FROM renters WHERE appartmentNumber = (?)
-    ;
-    """
+    cursor.execute("""DELETE FROM renters WHERE appartmentNumber = (?);"""
     , appartmentNumber
     )
 
 def clearTable():
-    cursor.execute("DROP TABLE renters")
+    cursor.execute("""--sql DROP TABLE renters;""")
     conn.commit()
 
 conn.close()
